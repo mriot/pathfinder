53264b73ae0af88dcfdbaf497ca9550073c7df78
mriot <kremer.markus1996@gmail.com>
2025-10-06 22:40:15 +0200

    improve command logging

--- src/core/bot.py
+++ src/core/bot.py
@@ -11,10 +11,12 @@ class PathfinderBot(discord.Bot):
         self.sm = settings_manager
 
     async def on_ready(self):
-        logging.info(f"Logged in as {self.user}")
+        logging.info(
+            f"Logged in as {self.user} ({f'DEBUG MODE on {self.debug_guilds}' if self.debug_guilds else 'PROD MODE'})"
+        )
         await self.sync_commands()
 
     async def on_application_command(self, ctx):
         logging.info(
-            f"{ctx.author} ({ctx.author.id}) used `/{ctx.command}` with options {ctx.selected_options}"
+            f"{ctx.author} ({ctx.author.id}) used `/{ctx.command}` with options {ctx.selected_options} in guild {ctx.guild.name} ({ctx.guild.id})"
         )
